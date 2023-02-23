71500f347ab114f97fa376664e86c6a9c477982e
Stefanos Stefanou <stefanos.stefanou@century.tech>
2023-02-23 08:45:52 +0000

    Timing MyIO

--- src/main/scala/lvl3/MyIO.scala
+++ src/main/scala/lvl3/MyIO.scala
@@ -1,24 +1,24 @@
 package lvl3
 
-case class MyIO[A](unsafeRun:()=>A){
-  def map[B](f:A=>B):MyIO[B]=MyIO(()=>f(unsafeRun()))
-  //def flatMap[B](f:A=>MyIO[B]):MyIO[B]=f(unsafeRun()) //this implementation immediately triggers execution,therefore makes the effect unlawful
-  def flatMap[B](f:A=>MyIO[B]):MyIO[B]=MyIO[B](()=>f(unsafeRun()).unsafeRun()) //this implementation does not immediately triggers execution
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.FiniteDuration
+
+case class MyIO[A](unsafeRun: () => A){
+  def map[B](fn:A=>B):MyIO[B] = MyIO(()=>fn(unsafeRun()))
+  def flatMap[B](fn:A=>MyIO[B]) :MyIO[B]= MyIO(()=>fn(unsafeRun()).unsafeRun())
 }
 
-object MyIO{
-  def apply[A](fn: () => A): MyIO[A] = new MyIO(fn)
-}
-
-object Main{
-  def main(args: Array[String]): Unit = {
-    val effect = MyIO[Unit](()=>println("Hello world"))
-    println("Effect created")
-    println("Executing effect...")
-    effect.unsafeRun()
-    println("Mapping effect to some other effect")
-    val mappedEffect =effect.map(_=>println("bye world!"))
-    println("Executing mapped effect")
-    mappedEffect.unsafeRun()
+object Timing extends App{
+  val clock:MyIO[Long]= MyIO[Long](()=>System.currentTimeMillis())
+  def time[A](action:MyIO[A]):MyIO[(FiniteDuration,A)] = {
+    for{
+      startTime <- clock
+      action <- action
+      endTime <- clock
+    }yield (FiniteDuration.apply(endTime-startTime,TimeUnit.MILLISECONDS),action)
+  }
+  val timedHello = Timing.time(MyIO(()=>println("Hello World")))
+  timedHello.unsafeRun() match {
+    case (duration,_)=>println(s"Took $duration ")
   }
 }
